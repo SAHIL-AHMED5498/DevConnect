0bdbb5cf912ff643e64014ca48256c0cd332824d
Sahil Ahmed <ahmedsahil5498@gmail.com>
2025-07-28 23:09:11 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -6,24 +6,55 @@ const app=express();
 
 app.use(express.json());
 
-app.use("/login",(req,res)=>{
-    res.send("youre in login page")
-})
-app.use("/createUser",(req,res)=>{
-    res.send("youre in create user page")
-})
 
-app.post("/api/data",(req,res)=>{
-    
-    console.log(JSON.stringify(req.body) );
-
-    res.status(201).json({message:"data recieved",data:req.body})
+//send user data
+app.get("/user/:id",(req,res)=>{
+//    console.log(req.query);             //for query
+//    console.log(req.params);          //for parameters
+    res.send({name:"sahil",age:24,id:req.params.id});
 })
 
-app.use("/",(req,res)=>{
+//create user data
+app.post("/user",(req,res)=>{
 
-    res.send("hello world");
+    //saved to db
+
+    res.send("saved successfully")
 })
+
+//delete user data
+app.delete("/user",(req,res)=>{
+
+    //delete from db
+
+    res.send("successfully deleted");
+
+})
+
+
+//multi route
+
+app.use("/multiRoutes",
+    (req,res,next)=>{
+        console.log("response handler 1");
+        next();
+    },
+    (req,res,next)=>{
+        console.log("response handler 2");
+        next();
+        console.log("this will print after response handler 3 out of execution stack");
+    },
+    (req,res,next)=>{
+        console.log("response handler 3");
+
+        res.send("response handler 3 ");
+        console.log("last line of response handler 3");
+
+    }
+)
+
+
+
 
 app.listen(3000,()=>{
     console.log("running on : http://localhost:3000");
